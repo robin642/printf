6361ba153215c99c568e57b99e472fa9889949bd
robin642 <wrobidean@gmail>
2023-07-24 14:17:35 -0700

    PRINTF PROJECTS

--- handle_print.c
+++ handle_print.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * handle_print - Prints an argument based on its type
  * @fmt: Formatted string in which to print the arguments.
@@ -12,11 +11,10 @@
  * @size: Size specifier
  * Return: 1 or 2;
  */
-
 int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
-	int w, unknow_len = 0, printed_chars = -1;
+	int i, unknow_len = 0, printed_chars = -1;
 	fmt_t fmt_types[] = {
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
@@ -24,11 +22,11 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
 		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
 	};
-	for (w = 0; fmt_types[w].fmt != '\0'; w++)
-		if (fmt[*ind] == fmt_types[w].fmt)
-			return (fmt_types[w].fn(list, buffer, flags, width, precision, size));
+	for (i = 0; fmt_types[i].fmt != '\0'; i++)
+		if (fmt[*ind] == fmt_types[i].fmt)
+			return (fmt_types[i].fn(list, buffer, flags, width, precision, size));
 
-	if (fmt_types[w].fmt == '\0')
+	if (fmt_types[i].fmt == '\0')
 	{
 		if (fmt[*ind] == '\0')
 			return (-1);
