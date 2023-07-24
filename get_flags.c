23b1592f9bb63fa1121172f1fe8f32d5ccf50afa
robin642 <wrobidean@gmail>
2023-07-24 14:08:23 -0700

    PRINTF PROJECTS

--- get_flags.c
+++ get_flags.c
@@ -6,30 +6,29 @@
  * @i: take a parameter.
  * Return: Flags:
  */
-
 int get_flags(const char *format, int *i)
 {
 	/* - + 0 # ' ' */
 	/* 1 2 4 8  16 */
-	int w, curr_d;
+	int j, curr_i;
 	int flags = 0;
 	const char FLAGS_CH[] = {'-', '+', '0', '#', ' ', '\0'};
 	const int FLAGS_ARR[] = {F_MINUS, F_PLUS, F_ZERO, F_HASH, F_SPACE, 0};
 
-	for (curr_d = *i + 1; format[curr_d] != '\0'; curr_d++)
+	for (curr_i = *i + 1; format[curr_i] != '\0'; curr_i++)
 	{
-		for (w = 0; FLAGS_CH[w] != '\0'; w++)
-			if (format[curr_d] == FLAGS_CH[w])
+		for (j = 0; FLAGS_CH[j] != '\0'; j++)
+			if (format[curr_i] == FLAGS_CH[j])
 			{
-				flags |= FLAGS_ARR[w];
+				flags |= FLAGS_ARR[j];
 				break;
 			}
 
-		if (FLAGS_CH[w] == 0)
+		if (FLAGS_CH[j] == 0)
 			break;
 	}
 
-	*i = curr_d - 1;
+	*i = curr_i - 1;
 
 	return (flags);
 }
