1ca5423e17c77609437323dae552bad1748ef2f5
robin642 <wrobidean@gmail>
2023-07-24 06:42:29 -0700

    PRINTF PROJECT

--- _printf.c
+++ _printf.c
@@ -9,7 +9,7 @@ void print_buffer(char buffer[], int *buff_ind);
  */
 
 int _printf(const char *format, ...)
-{	
+{
 	int r, prints = 0, printed_charz = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
