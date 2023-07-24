0a8988e600eaf321dbca6f034802545043fb7f8d
robin642 <wrobidean@gmail>
2023-07-24 14:03:33 -0700

    PRINTF PROJECTS

--- get_precision.c
+++ get_precision.c
@@ -8,28 +8,26 @@
  *
  * Return: Precision.
  */
-
 int get_precision(const char *format, int *i, va_list list)
-
 {
-	int curr_d = *i + 1;
+	int curr_i = *i + 1;
 	int precision = -1;
 
-	if (format[curr_d] != '.')
+	if (format[curr_i] != '.')
 		return (precision);
 
 	precision = 0;
 
-	for (curr_d += 1; format[curr_d] != '\0'; curr_d++)
+	for (curr_i += 1; format[curr_i] != '\0'; curr_i++)
 	{
-		if (is_digit(format[curr_d]))
+		if (is_digit(format[curr_i]))
 		{
 			precision *= 10;
-			precision += format[curr_d] - '0';
+			precision += format[curr_i] - '0';
 		}
-		else if (format[curr_d] == '*')
+		else if (format[curr_i] == '*')
 		{
-			curr_d++;
+			curr_i++;
 			precision = va_arg(list, int);
 			break;
 		}
@@ -37,7 +35,7 @@ int get_precision(const char *format, int *i, va_list list)
 			break;
 	}
 
-	*i = curr_d - 1;
+	*i = curr_i - 1;
 
 	return (precision);
 }
